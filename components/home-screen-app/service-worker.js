3d12fc632596668f27a1a2ef749d93381d089ed1
markusdoppler <markusdoppler@me.com>
2021-05-30 12:31:50 +0200

    COMPONENTS home screen app

---
+++ components/home-screen-app/service-worker.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('butterfly-store').then((cache) => cache.addAll([
+      '/snippets/components/home-screen-app/',
+      '/snippets/components/home-screen-app/index.html',
+      '/snippets/components/home-screen-app/index.js',
+      '/snippets/components/home-screen-app/style.css',
+      '/snippets/assets/peacock-300.png',
+    ])),
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
+});
